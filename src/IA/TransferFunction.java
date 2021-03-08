0aae50dfde0a93c72a153ded21f4132a003b0c54
Samy Hoffmann <samyhoffmann01@gmail.com>
2021-03-08 16:43:49 +0100

    Premier commit ajout des fichiers pour l'IA

---
+++ src/IA/TransferFunction.java
@@ -0,0 +1,6 @@
+package IA;
+
+public interface TransferFunction{
+	public double evalute(double value);
+	public double evaluteDerivate(double value);
+}
---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module ProjetMorpionL3 {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens sample;
+}
